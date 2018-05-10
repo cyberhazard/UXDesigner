c8c95cd090b41c5aa1f35334cccd49f957d9d0cb
Zolotukhin Anatoliy <zolotukhinwebpr@mail.ru>
2018-05-10 18:58:37 +0700

    input color

--- src/index.js
+++ src/index.js
@@ -7,11 +7,13 @@ colorSwitch.addEventListener('change', e => {
   if (e.target.checked) {
     root.style.setProperty('--primaryColor', 'black')
     root.style.setProperty('--secondaryColor', 'white')
+    root.style.setProperty('--inputPrimaryColor', '#DDDDDD')
     root.style.setProperty('--logoImage', `url(${require('./images/logo-white.svg')})`)
     root.style.setProperty('--switchImage', `url(${require('./images/Sun.svg')})`)
   } else {
     root.style.setProperty('--primaryColor', '')
     root.style.setProperty('--secondaryColor', '')
+    root.style.setProperty('--inputPrimaryColor', '')
     root.style.setProperty('--logoImage', '')
     root.style.setProperty('--switchImage', '')
   }
