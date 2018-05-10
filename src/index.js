2e50750721dd42c1cf4644757b6ca8385f7ff746
Zolotukhin Anatoliy <zolotukhinwebpr@mail.ru>
2018-05-10 17:45:41 +0700

    api + fix footer for safari

--- src/index.js
+++ src/index.js
@@ -30,6 +30,7 @@ const subscribe = () => {
   const form = document.querySelector('.Subscribe__form');
   const input = document.querySelector('.Subscribe__input');
   const alert = document.querySelector('.Subscribe__alert');
+  const api = 'https://api.unisender.com/ru/api/subscribe?format=json&api_key=6wkhfat5zcsi54416q9hxe634i6gso4x7wf7m4xy&list_ids=13433066&fields[email]='
   const regular = /\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b/gi;
 
   const validate = (el) => {
@@ -38,19 +39,18 @@ const subscribe = () => {
       alert.innerHTML = "* Incorrect e-mail address! Verify that the input is correct";
       setTimeout(_ => { alert.innerHTML = "" }, 3000);
     } else {
-      sendMail().then(
+      var value = document.querySelector('.Subscribe__input').value;
+      sendMail(api,value).then(
         alert.style.color = "var(--primaryColor)",
         alert.innerHTML="You have successfully subscribed, thank you!",
-        setTimeout(_ => { alert.innerHTML = "" }, 3000),
+        setTimeout(_ => { alert.innerHTML = "" }, 1000),
         form.reset()
       );
     }
   }
-
-  const sendMail = function() {
-    return fetch('https://cp.unisender.com/ru/subscribe?hash=6wcacndtmdefaaaa4xyx69fwniggntno9t7wnz7m1wqgx3jehekto', {
-      method: 'POST',
-      body: new FormData(form)
+  const sendMail = function(api,value) {
+    return fetch(api+value+'&double_optin=3', {
+      method: 'POST'
     })
   };
 
@@ -60,3 +60,4 @@ const subscribe = () => {
   }
 }
 subscribe();
+// https://api.unisender.com/ru/api/subscribe?format=json&api_key=6wkhfat5zcsi54416q9hxe634i6gso4x7wf7m4xy&list_ids=13433066&fields[email]=EMAIL
